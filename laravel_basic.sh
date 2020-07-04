9b8742f860f8dd8e43975944005c613443960869
sheriffmarley <36710078+sheriffmarley@users.noreply.github.com>
2020-07-04 23:43:26 +0200

    Update laravel_basic.sh

--- laravel_basic.sh
+++ laravel_basic.sh
@@ -4,6 +4,9 @@ hostname=${hostname:-example.com}
 read -p "PHP Version? [7.4]: " phpversion
 phpversion=${phpversion:-7.4}
 
+read -p "Laravel path? /var/www/html/laravel/public: " laravelpath
+laravelpath=${laravelpath:-/var/www/html/laravel/public}
+
 cat > /etc/nginx/conf.d/$hostname.conf << EOL
 
     server {
@@ -20,9 +23,8 @@ cat > /etc/nginx/conf.d/$hostname.conf << EOL
         ssl_session_cache shared:SSL:10m;
         ssl_session_timeout  30m;
         ssl_session_tickets off;
-        #ssl_ciphers HIGH:!aNULL:!MD5;
-        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
-        ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:ECDHE-RSA-AES128-GCM-SHA256:AES256+EECDH:DHE-RSA-AES128-GCM-SHA256:AES256+EDH:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4";
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
         ssl_prefer_server_ciphers on;
         ssl_stapling on;
         ssl_stapling_verify on;
@@ -33,7 +35,7 @@ cat > /etc/nginx/conf.d/$hostname.conf << EOL
 
         location / {
 
-            root /var/www/html/fs/public;
+            root $laravelpath;
             index index.php;
 
             try_files \$uri \$uri/ /index.php?\$query_string;
