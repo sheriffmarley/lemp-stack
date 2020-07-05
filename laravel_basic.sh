9d3075e9a333414f06629ee88ed2f0bf4f4d7eba
sheriffmarley <36710078+sheriffmarley@users.noreply.github.com>
2020-07-05 19:19:37 +0200

    Update laravel_basic.sh

--- laravel_basic.sh
+++ laravel_basic.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-read -p "Host name? [example.com]: " hostname
+read -p "Host name (including subdomain if wanted)? [example.com]: " hostname
 hostname=${hostname:-example.com}
 
 read -p "PHP Version? [7.4]: " phpversion
 phpversion=${phpversion:-7.4}
 
-read -p "Laravel path? /var/www/html/laravel/public: " laravelpath
-laravelpath=${laravelpath:-/var/www/html/laravel/public}
+read -p "Root path? /var/www/html/laravel/public: " rootpath
+rootpath=${rootpath:-/var/www/html/laravel/public}
+
+ORIGINCERT=/etc/nginx/certs/cloudflare-origin.crt
+if [ ! -f "$ORIGINCERT" ]; then
+    
+    mkdir -p /etc/nginx/certs
+
+    echo "Download cf origin cert"
+    wget https://support.cloudflare.com/hc/en-us/article_attachments/360044928032/origin-pull-ca.pem
+
+    mv origin-pull-ca.pem $ORIGINCERT
+fi
+
+dhparam=/etc/nginx/certs/dhparam-2048.pem
+if [ ! -f "$dhparam" ]; then
+
+    echo "Create dhparam file $dhparam"
+    openssl dhparam -out $dhparam 2048
+fi
 
 cat > /etc/nginx/conf.d/$hostname.conf << EOL
 
@@ -19,9 +37,9 @@ cat > /etc/nginx/conf.d/$hostname.conf << EOL
         listen       443 ssl default_server;
         #add_header Strict-Transport-Security "max-age=31536000; includeSubdomains; preload";
 
-        ssl_certificate         "/usr/local/nginx/conf/cert/ssl-bundle.crt";
-        ssl_certificate_key     "/usr/local/nginx/conf/cert/private/server.key";
-        ssl_dhparam cert/dhparam.pem;
+        ssl_certificate         /etc/nginx/certs/server.crt;
+        ssl_certificate_key     /etc/nginx/certs/private/server.key;
+        ssl_dhparam $dhparam;
         ssl_session_cache shared:SSL:10m;
         ssl_session_timeout  30m;
         ssl_session_tickets off;
@@ -32,22 +50,28 @@ cat > /etc/nginx/conf.d/$hostname.conf << EOL
         ssl_stapling_verify on;
         resolver 1.1.1.1 8.8.8.8 8.8.4.4 valid=300s;
         resolver_timeout 5s;
+        
+        ssl_client_certificate $ORIGINCERT;
+        ssl_verify_client on;
 
         index index.php index.html;
 
+        root $rootpath;
+            
+        error_log /var/log/nginx/$subdomain.error.log;
+        access_log /var/log/nginx/$subdomain.access.log;
+        
         location / {
-
-            root $laravelpath;
-            index index.php;
-
             try_files \$uri \$uri/ /index.php?\$query_string;
-
-             location ~ \.php\$ {
-                        fastcgi_pass   unix:/run/php/php${phpversion}-fpm.sock;
-                        fastcgi_index  index.php;
-                        fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
-                        include        fastcgi_params;
-             }
+        }
+        
+        location ~ \.php\$ {
+            try_files $uri =404;
+            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            fastcgi_pass   unix:/run/php/php${phpversion}-fpm.sock;
+            fastcgi_index  index.php;
+            fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
+            include        fastcgi_params;
         }
 
     }
