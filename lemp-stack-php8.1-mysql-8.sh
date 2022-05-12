0ff9d709087e82aad1dadd337db4842b54fe5a07
sheriffmarley <36710078+sheriffmarley@users.noreply.github.com>
2022-05-12 13:56:38 +0200

    Update lemp-stack-php8.1-mysql-8.sh

--- lemp-stack-php8.1-mysql-8.sh
+++ lemp-stack-php8.1-mysql-8.sh
@@ -28,10 +28,9 @@ sudo apt -y update
 sudo apt install -y mysql-server
 
 # PHP 8.1
-sudo apt -y install lsb-release apt-transport-https ca-certificates 
-sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt update
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php
 
 sudo apt update -y
 
